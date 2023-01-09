6e6e7da645f8a3e1190cb12602bfabd885eecdc2
Dana Ng <ngdana1@gmail.com>
2023-01-09 03:06:56 -0800

    update styling

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="flex flex-row justify-evenly items-center content-center p-5">
-    <h1 class="text-6xl font-bold tracking-tight">
-      Ramen Recommendation Engine
-    </h1>
-    <img src="/ramen.png" class="w-20 h-20" />
+  <div class="px-6 lg:px-8">
+    <div
+      class="mx-auto max-w-3xl pt-10 pb-20 sm:pt-5 sm:pb-10 flex flex-row justify-evenly items-center content-center"
+    >
+      <h1 class="text-4xl font-bold tracking-tight text-teal-500">
+        Ramen Recommendation Engine
+      </h1>
+      <img src="/ramen.png" class="w-10 h-10" />
+    </div>
   </div>
 </template>
 
