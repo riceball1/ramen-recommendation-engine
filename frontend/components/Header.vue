ab0552c788a2a12170d1030e70ffe7a6e3dbc054
Dana Ng <14253024+riceball1@users.noreply.github.com>
2023-01-09 19:29:55 -0800

    Implement Landing Page (#16)

---
+++ frontend/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="px-6 lg:px-8">
+    <div
+      class="mx-auto max-w-3xl pt-10 pb-20 sm:pt-5 sm:pb-10 flex flex-row justify-evenly items-center content-center"
+    >
+      <h1 class="text-4xl font-bold tracking-tight text-teal-500">
+        Ramen Recommendation Engine
+      </h1>
+      <img src="/ramen.png" class="w-10 h-10" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Header",
+});
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,37 @@
 <template>
-  <p>Ramen Recommendation Engine</p>
+  <div class="bg-black text-slate-50 h-screen">
+    <Header />
+    <main>
+      <div class="px-6 lg:px-8">
+        <div class="mx-auto max-w-3xl pt-15 pb-32 sm:pt-24 sm:pb-40">
+          <h2
+            class="text-4xl font-bold tracking-tight sm:text-center sm:text-4xl"
+          >
+            Hungry for ramen?
+          </h2>
+          <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center text-teal-500">
+            Too many choices can stop us in our tracks. That's why we're here to
+            help you figure out what kind of ramen will help fill your tummy.
+          </p>
+          <div class="mt-8 flex gap-x-4 sm:justify-center">
+            <a
+              href="#"
+              class="inline-block rounded-lg bg-teal-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-teal-200 hover:bg-black hover:ring-teal"
+            >
+              Take the ramen quiz!
+              <span class="text-white" aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
